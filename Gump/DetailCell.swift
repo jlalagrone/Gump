d7ca63b8d5b6b43a14919d86561ff40b55b9d455
Jordan LaGrone <ballingincode@gmail.com>
2020-04-21 23:54:27 -0500

    Added DetailController

---
+++ Gump/DetailCell.swift
@@ -0,0 +1,9 @@
+//
+//  DetailCell.swift
+//  Gump
+//
+//  Created by Jordan LaGrone on 4/21/20.
+//  Copyright © 2020 JordanLaGrone. All rights reserved.
+//
+
+import Foundation
---
+++ Gump/DetailController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailController.swift
+//  Gump
+//
+//  Created by Jordan LaGrone on 4/21/20.
+//  Copyright © 2020 JordanLaGrone. All rights reserved.
+//
+
+import UIKit
+
+class DetailController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
